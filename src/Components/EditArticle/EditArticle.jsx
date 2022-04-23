f91799eb86ba690135bc99365cc13fee887300e4
Tosha-666 <qwerty21042018@mail.ru>
2022-04-23 16:44:45 +0300

    inputForms

--- src/Components/EditArticle/EditArticle.jsx
+++ src/Components/EditArticle/EditArticle.jsx
@@ -1,86 +1,86 @@
 import React, { useState } from 'react'
-import { useForm, FormProvider } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 import './EditArticle.scss'
 import { TagsInput } from '../TagsInput'
 
 const EditArticle = () => {
-  const { register, handleSubmit, reset } = useForm()
-  const methods = useForm()
+  const { register, handleSubmit, reset, control } = useForm()
   const [tagInput, setTagsInput] = useState([''])
   let k = 1
-  const addTag = (tag) => {
+  const addTag = (e) => {
+    e.preventDefault()
     setTagsInput((prevstate) => {
-      return [tag, ...prevstate]
+      console.log(prevstate)
+      return [...prevstate, '']
     })
   }
   const editArticle = (data) => {
     console.log(data)
   }
+  // const deleteTag = (id) => {
+  //   tagInput
+  // }
   return (
     <div className="edit-article-container">
       <h1 className="edit-article-title">Edit article</h1>
-      <FormProvider {...methods}>
-        <form
-          className="edit-article-form"
-          onSubmit={handleSubmit(editArticle)}
-        >
-          <label className="edit-article-form-label" htmlFor="title">
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            placeholder="Title"
-            {...register('title')}
-            className="edit-article-form-content"
-          />
-          <label className="edit-article-form-label" htmlFor="description">
-            Short description
-          </label>
-          <input
-            type="text"
-            name="description"
-            id="description"
-            placeholder="Title"
-            {...register('description')}
-            className="edit-article-form-content"
-          />
 
-          <label className="edit-article-form-label" htmlFor="text">
-            Text{' '}
-          </label>
-          <textarea
-            type="text"
-            id="text"
-            placeholder="Text"
-            className="edit-article-form-content"
-            {...register('text')}
-          />
+      <form className="edit-article-form" onSubmit={handleSubmit(editArticle)}>
+        <label className="edit-article-form-label" htmlFor="title">
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Title"
+          {...register('title')}
+          className="edit-article-form-content"
+        />
+        <label className="edit-article-form-label" htmlFor="description">
+          Short description
+        </label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          placeholder="Title"
+          {...register('description')}
+          className="edit-article-form-content"
+        />
 
-          <label className="edit-article-form-label" htmlFor="tag">
-            Tags
-          </label>
-          <div>
-            {tagInput.map((tag) => (
-              <TagsInput
-                tagInner={tag}
-                key={k++}
-                addTag={addTag}
-                register={register}
-                id={k++}
-              />
-            ))}
-          </div>
+        <label className="edit-article-form-label" htmlFor="text">
+          Text{' '}
+        </label>
+        <textarea
+          type="text"
+          id="text"
+          placeholder="Text"
+          className="edit-article-form-content"
+          {...register('text')}
+        />
 
-          <input
-            type="submit"
-            className="edit-article-form-send"
-            value="Send"
-          />
-        </form>
-      </FormProvider>
+        <label className="edit-article-form-label" htmlFor="tag">
+          Tags
+        </label>
+        <div>
+          {tagInput.map((tag) => (
+            <TagsInput
+              tagInner={tag}
+              key={k++}
+              addTag={addTag}
+              register={register}
+              id={k++}
+              control={control}
+            />
+          ))}
+          <button className="edit-article-tags-add" onClick={addTag}>
+            Add Tag
+          </button>
+        </div>
+
+        <input type="submit" className="edit-article-form-send" value="Send" />
+      </form>
     </div>
   )
 }
--- src/Components/TagsInput/TagsInput.jsx
+++ src/Components/TagsInput/TagsInput.jsx
@@ -1,36 +1,49 @@
-import React, { useState, useEffect } from 'react'
-import { useFormContext } from 'react-hook-form'
+import React, { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 
-const TagsInput = ({ tagInner, addTag, id }) => {
-  const { register } = useFormContext()
-  const [tag, setTag] = useState('')
-  //   useEffect(() => {
-  //     setTag(tagInner)
-  //   }, [])
-  const onAdded = (e) => {
-    e.preventDefault()
-    addTag(tag)
-  }
+const TagsInput = ({ tagInner, addTag, id, control }) => {
+  // const {
+  //   field: { onChange, ref },
+  // } = useController({
+  //   control,
+  // })
+
+  const [tag, setTag] = useForm({1})
+  // useEffect(() => {
+  //   setTag(tagInner)
+  // }, [])
+  // const onAdded = (e) => {
+  //   e.preventDefault()
+  //   addTag(tag)
+  // }
   return (
-    <div>
-      <input
-        type="text"
-        id="tag"
-        className="edit-article-form-content edit-article-form-tag"
-        value={tag}
-        onChange={(event) => setTag(event.target.value)}
-        // {...register('tags')}
+    <React.Fragment>
+      <Controller
+      // defaultValue={}
+        control={control}
+        name={`tag${id}`}
+        render={({ field: { onChange, value } }) => (
+          <input
+            type="text"
+            id="tag"
+            className="edit-article-form-content edit-article-form-tag"
+            value={tag}
+            onChange={setTag}
+            // (event) => setTag(event.target.value)}
+          />
+        )}
       />
+
       <button
         className="edit-article-tags-delete"
         //   onClick={DeleteTag}
       >
         Delete
       </button>
-      <button className="edit-article-tags-add" onClick={onAdded}>
+      {/* <button className="edit-article-tags-add" onClick={onAdded}>
         Add Tag
-      </button>
-    </div>
+      </button> */}
+    </React.Fragment>
   )
 }
 export default TagsInput
